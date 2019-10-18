39baf22737e4cc162b2d58cf4a3799f5a3b0d54c
biller0 <1718592645@qq.com>
2019-10-18 08:00:43 +0800

    work6

--- work6/pai.js
+++ work6/pai.js
@@ -1,31 +1,31 @@
 function calculate(){
-    let num = document.getElementById('?').value
+    let num = document.getElementById('num').value
     num = parseInt(num)&&Number(num)
     if(!isNaN(num)){
-        document.getElementById(?).value = pi(?)
+        document.getElementById('pai').value = pi(num)
     }
 }
 function factorial(n){
     let sum = 1
     for(let i=1;i<=n;++i){
-      sum
+      sum = sum*i
     }
     return sum
 }
 function oddFactorial(n){
     let sum = 1
-    for(let i=1;i>n;++i){
-        
+    for(let i=1;i<=n;++i){
+      sum = sum*(2*i+1)
     }
     return sum
 }
 function pi(num){
     let sum = 0
     for(let i=0;i<=num;++i){
-        let dividend = factorial()
-        let divisor = oddFactorial()
-
+      let dividend = factorial(i)
+      let divisor = oddFactorial(i)
+      sum = sum+(dividend/divisor)
     }
-    return
+    return sum * 2
 }
-calculate
+calculate()
