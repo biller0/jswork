40b72d3ba1e718a6e5bf38c3b36a55ab5ee1bbc4
biller0 <1718592645@qq.com>
2019-10-21 14:51:41 +0800

    work7

--- work7silde/utils.js
+++ work7silde/utils.js
@@ -37,30 +37,30 @@ function inserSort(str){
     let sortLog = []
     for (let i = 1; i < arr.length; i++){
         for(let j =i; j > 0; j--){
-            sortLog.push([arr.concat(), [j, j + 1]])
-            sortLog.push([arr.concat(), [j, j + 1]])
-            sortLog.push([arr.concat(), [j, j + 1]])
+            sortLog.push([arr.concat(), [j, j - 1]])
+            sortLog.push([arr.concat(), [j, j - 1]])
+            sortLog.push([arr.concat(), [j, j - 1]])
             if (arr[j - 1] > arr[j]){
             [arr[j -1],arr[j]] = [arr[j], arr[j - 1]]
-            sortLog.push([arr.concat(), [j, j + 1]])
             }
         }
     }
     sortLog.push([arr.concat(), [-1, -1]])
     return sortLog
 }
-function showLog(logValues,showElement){
+
+function showLog(logValues, showElement) {
     let str = ''
-    let{
+    let {
         done,
-        value: [row,pos]
+        value: [row, pos]
     } = logValues.next()
     for (const key in row) {
-        let color = pos.indexOf(Number(key)) > -1 ?'color:red;' : ''
+        let color = pos.indexOf(Number(key)) > -1 ? 'color:red;' : ''
         str += '<span style="font-size:' + row[key] * 20 + 'px;' +
-                color + '">' + row[key] + '</span>'
+            color + '">' + row[key] + '</span>'
     }
     showElement.innerHTML = str
     if (pos[0] != -1)
-    setTimeout("showLog(logValues,showDiv)", 500)
+        setTimeout("showLog(logValues,showDiv)", 500)
 }
