0b8f8a55d23e287a179ff601a9c01ecc855fbc25
biller0 <1718592645@qq.com>
2019-11-01 10:52:00 +0800

    work10

--- work10/index.js
+++ work10/index.js
@@ -2,20 +2,20 @@ function monkey(){
     let total = document.getElementById("monkeytotal").value
     let kick = document.getElementById("monkeykick").value
     total = parseInt(total) && Number(total)
-    total = parseInt(kick) && Number(kick)
+    kick = parseInt(kick) && Number(kick)
     if(isNaN(total)||isNaN(kick)){
         alert('请输入数字')
         return
     }
     let monkey=[]
-    for(let i=1;i<=total;i++){
+    for (let i=1;i<=total;i++){
         monkey.push(i)
     }
     let i=0
-    while(monkey.length>1){
+    while (monkey.length>1){
         i++;
         head = monkey.shift()
-        if(i%kick != 0){
+        if(i%kick!=0){
             monkey.push(head);
         }
     }
